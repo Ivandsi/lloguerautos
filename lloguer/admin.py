aa726232d93a05189c06b700b3e1ce492cb92993
Ivan D.S. <ivandiazsanchez1996@gmail.com>
2025-03-28 15:42:29 +0100

    Fita 2

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from lloguer.models import *
 
 # Register your models here.
+
+class AutomobilAdmin(admin.ModelAdmin):
+    list_display = ("marca", "model", "matricula")
+
+admin.site.register(Automobil, AutomobilAdmin)
