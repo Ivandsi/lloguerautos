62c6bb2364045686a57afc12d94f44984a91e730
Ivan D.S. <ivandiazsanchez1996@gmail.com>
2025-03-28 16:28:18 +0100

    Fita 5

--- lloguer/views.py
+++ lloguer/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from lloguer.models import *
 
 # Create your views here.
+def view_autos(request):
+    # Obtenim tots els automòbils de la base de dades
+    automobils = Automobil.objects.all()
+    
+    # Retornem la plantilla amb la llista d'automòbils
+    return render(request, 'autos.html', {'automobils': automobils})
